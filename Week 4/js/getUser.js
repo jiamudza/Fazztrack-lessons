832b71cbd329db6c024801da376e13aab881db8b
jiamudza <jiamudza@gmail.com>
2022-12-24 00:24:41 +0700

    feat: add feature to get data to API with fetch

--- Week 4/js/getUser.js
+++ Week 4/js/getUser.js
@@ -1,9 +1,25 @@
+const getEmail = document.querySelector('#email-input');
+const getPassword = document.querySelector('#password-input');
+const getNumber = document.querySelector('#number-input')
+const addNewUser = document.querySelector(".form-for-regist");
 const url = "https://63a5a93a318b23efa79a1907.mockapi.io/userAccount"
 
-fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        data.forEach(data => {
-            console.log(data)
-        });
+addNewUser.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    fetch(url, {
+        method: "POST",
+        headers: {
+            'tittle' : 'user API'
+        },
+        body:
+            {
+                email: getEmail.value,
+                password: getPassword.value,
+                number: getNumber.value
+            }
+        
     })
+    .then(response => console.log(response.json()))
+    .catch(err => err);
+})
