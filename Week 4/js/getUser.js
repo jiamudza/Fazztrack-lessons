1263f968029a7b4a3827ea8c6459a99d2de6537f
jiamudza <jiamudza@gmail.com>
2022-12-27 11:18:49 +0700

    fix bug post methof

--- Week 4/js/getUser.js
+++ Week 4/js/getUser.js
@@ -1,25 +1,27 @@
-const getEmail = document.querySelector('#email-input');
-const getPassword = document.querySelector('#password-input');
-const getNumber = document.querySelector('#number-input')
 const addNewUser = document.querySelector(".form-for-regist");
 const url = "https://63a5a93a318b23efa79a1907.mockapi.io/userAccount"
 
 addNewUser.addEventListener('submit', (e) => {
     e.preventDefault();
+
+    const getEmail = document.querySelector('#email-input').value;
+    const getPassword = document.querySelector('#password-input').value;
+    const getNumber = document.querySelector('#number-input').value;
     
     fetch(url, {
         method: "POST",
         headers: {
-            'tittle' : 'user API'
+            'Content-Type' : 'application/json'
         },
-        body:
+        body: JSON.stringify(
             {
-                email: getEmail.value,
-                password: getPassword.value,
-                number: getNumber.value
+                email: getEmail,
+                password: getPassword,
+                number: getNumber
             }
-        
+        )
+               
     })
-    .then(response => console.log(response.json()))
+    .then(response => response.json())
     .catch(err => err);
 })
