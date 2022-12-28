48845402b74c7340995e6624023f96e8ae7a7d69
jiamudza <jiamudza@gmail.com>
2022-12-28 14:14:19 +0700

    add 'PUT' method to fetch api

--- Week 4/js/CRUD.js
+++ Week 4/js/CRUD.js
@@ -1,5 +1,11 @@
 const wrapper = document.querySelector('#wrapper');
 const url = 'https://63a5a93a318b23efa79a1907.mockapi.io/userAccount';
+const update = document.querySelector('#submit-update')
+const cancel = document.querySelector('#cancel-update')
+const modal = document.querySelector("#modal");
+let emailUp = document.querySelector("#email-up")
+let passwordUp = document.querySelector("#password-up")
+let numberUp = document.querySelector("#number-up")
 let output = "";
 fetch(url)
     .then(response => response.json())
@@ -7,9 +13,12 @@ fetch(url)
         data.map(data => {
             output +=  `
                 <div class="rounded-min shd-pure2" id="card" data-id=${data.id}> 
-                    <h4 class="px-15" id="email">email : ${data.email}</h4>
-                    <p class="px-15" id="password">password : ${data.password}</p>
-                    <p class="px-15" id="number">number : ${data.number}</p>
+                    <label for="email">Email:</label>
+                    <h4 class="px-15" id="email">${data.email}</h4>
+                    <label for="password">Password:</label>
+                    <p class="px-15" id="password">${data.password}</p>
+                    <label for="number">Number:</label>
+                    <p class="px-15" id="number">${data.number}</p>
                     <button type="delete" class="btn" id="delete-post">delete</button>
                     <button class="btn" id="edit-post">edit</button>
                 </div>`
@@ -32,5 +41,40 @@ wrapper.addEventListener('click', (event) => {
         .then(response => response.json())
         .then(() => location.reload())
         
+    }
+
+    if(editButtonClicked) {
+        modal.removeAttribute('class')
+        const parent =event.target.parentElement;
+
+        let emailContent = parent.querySelector('#email').textContent;
+        let passwordContent = parent.querySelector('#password').textContent;
+        let numberContent = parent.querySelector('#number').textContent;
+
+        emailUp.value = emailContent;
+        passwordUp.value = passwordContent;
+        numberUp.value = numberContent;
+
+        update.addEventListener('click', (e) => {
+            e.preventDefault()
+                fetch(`${url}/${id}`, {
+                method: "PUT",
+                headers: {
+                    'Content-Type' : 'application/json'
+                },
+                body: JSON.stringify({
+                    email: emailUp.value,
+                    password: passwordUp.value,
+                    number: numberUp.value
+                })
+            })
+            .then(res => res.json())
+            .then(modal.setAttribute('class','d-none'))
+        })
+        cancel.addEventListener('click', (e) => {
+            e.preventDefault()
+            modal.setAttribute('class', 'd-none')
+        })
+
     }
 })
